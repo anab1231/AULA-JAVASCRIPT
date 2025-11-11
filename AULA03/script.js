a56d8d1e4b59160d0b793e61ea336adcb7a13584
anab1231 <programadorsistemas0@gmail.com>
2025-11-10 21:36:14 -0300

    cotinuacao

---
+++ AULA03/script.js
@@ -0,0 +1,5 @@
+// veja abaixo exemplo de como e quando usar as crases
+var nome = 'ana';
+var idade = 20; //numero usados sem aspas (simples ou duplas)
+var mensagem =`olá , meu nome é  ${nome} e tenho ${idade} anos`;
+console.log(mensagem); // utilizando para fazer a impressão do resultado na tela 
