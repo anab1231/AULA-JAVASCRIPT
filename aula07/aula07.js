b72d901554052d5d2806d12cdfae4e2b577402f6
anab1231 <programadorsistemas0@gmail.com>
2025-11-14 21:40:02 -0300

    aula01

--- aula07/aula07.js
+++ aula07/aula07.js
@@ -1,12 +1,31 @@
-const lista = document.getElementsByClassName("list");
+const lista = document.getElementById("list");
 
-function adicionaritem()
-{
+function adicionaritem() {
+    // criar um novo elemento de "li" (item de lista ) e o armazenar na variavel "novaLI"
+    const novaLI = document.createElement("li");
+    novaLI.innerText = "novo item da lista ";
+    // define o textto do novo item ,este texto será exibindo no navegador como conteudo da "nova li "
 
-    
-    
+    lista.appendChild(novaLI);
+    // adicionar o novo item (novali) como p ultimo filho do elemento 'lista',isso faz com que o item seja exibido corretamente dentro da ul (lista ).
+    // o appendchild acrescenta o elemento selecionado , o que for desclarando na sequencia entre parenteses (appendchild(novali)) .
+
+
+
 }
-function removeritem(){
+function removeritem() {
+    const ultimoitem = lista.lastElementChild;
+    // selecionado o ultimo elemento filho dentro de "lista" e o armazena na variavel 'ultimoitem'
+    // lastelementchild retorna o ultimo elemento diretamente contido em "lista" que é o ultimo item da lista 
+    lista.removeChild(ultimoitem);
+    // remove o ultimo item (ultimoitem) de dentro de  "lista"
+    // removechild é um metodo que remove o elemento filho especificado.
+
+
+
+
+
+
 
 
 
