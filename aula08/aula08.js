8019593c9099d9c09242a62a4e0123bd7bc325fc
anab1231 <programadorsistemas0@gmail.com>
2025-11-25 21:02:25 -0300

    aula

---
+++ aula08/aula08.js
@@ -0,0 +1,16 @@
+
+// "IF" é uma estrutura usada tomar decisoes no codigo
+// ele verificar uma condicão (algo que pode ser verdadeiro ou falso )
+// e executa um bloco de codigo caso essa codicão seja verdadeira 
+let idade = 25;
+if (idade >=18) { // aqui , o js pregunta :idade é maior ou igaul a 18 se sim , ele executar o que esta dentro das {}
+    console.log("voce é maior de idade ");
+}
+else {
+    console.log ("voce é menor de idade");
+}
+
+
+// o "ELSE" "É USADO QUANDO DIZER : 
+// SE A CODICÃO DE IF NAO FOR VERDADEIRA , FACA OUTRA COISA" 
+// SE O IF FOR FALSO AUTOMATICAMENTE O ELSE É EXECUTADO  
---
+++ aula09/aula09.js
@@ -0,0 +1,8 @@
+let interruptor = "desligado";// 
+if (interruptor === "ligado") {
+console.log ("A luz esta acesa");
+
+}
+else{
+    console.log("A luz esta apagada"); 
+}
